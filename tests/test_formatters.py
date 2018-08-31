95dc82a2f8a883b600fc5e36dae2275d98904c9b
Maciej Sobczak <sbcm@viessmann.com>
2018-08-31 13:15:10 +0200

    fix formatter bug

--- tests/test_formatters.py
+++ tests/test_formatters.py
@@ -21,3 +21,18 @@ class JsonFormatterTestCase(TestCase):
             'hi': 'all',
             'method_calls': [],
         }
+
+    def test_add_field__broken_args(self):
+
+        record = Mock(args=('606909c4', 'PUT', '/wat'))
+
+        message_dict = {'message': 'here'}
+        log_record = {'hello': 'there'}
+
+        JsonFormatter().add_fields(log_record, record, message_dict)
+
+        assert log_record == {
+            'hello': 'there',
+            'message': 'here',
+            'method_calls': [],
+        }
